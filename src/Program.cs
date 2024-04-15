4accfffbfe4de9f12d8fde7359a2f1e8a3fd2760
Ferdinand Sukhoi <ferdinandsukhoi@outlook.com>
2024-04-15 09:33:54 +0800

    mcs ping-pong v2

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,9 @@
-
 using Microsoft.AspNetCore.Mvc;
 using Steeltoe.Extensions.Configuration.Placeholder;
 using System.Net;
+
 ServicePointManager.DnsRefreshTimeout = 0; // Force DNS Refresh
 var builder = WebApplication.CreateBuilder(args);
-
 
 
 builder.Configuration.AddPlaceholderResolver();
@@ -21,68 +20,40 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapPost("/ping", async (
-    [FromBody] PingMessage message,
-    [FromServices] HttpClient http,
-    [FromServices] IConfiguration config,
-    HttpContext context) =>
-{
-    if (message.Ttl <= 0) return Results.BadRequest();
-    var appUrl = config["AppUrl"];
-    var insId = config["InstanceId"] ?? "<Id-Not-Set>";
-    var localIp = context.Connection.LocalIpAddress?.ToString() ?? "<UNKNOWN>";
-    var thisNode = new NodeInfo(insId, localIp);
-    
-    if (message.Ttl == 1)
-    {
-        return Results.Ok(
-            new PongMessage(message.Count + 1, message.Initial, message.Sender, thisNode));
-    }
-    else
-    {
-        var response = await http.PostAsJsonAsync(appUrl,
-            new PingMessage(message.Ttl - 1, message.Count + 1, message.Initial, thisNode))
-            ;
-        return Results.Ok(await response.Content.ReadFromJsonAsync<PongMessage>());
-    }
-}).WithName("Ping").WithOpenApi();
+app.MapGet
+    (
+        "/",
+        async
+        (
+            [FromQuery] int ttl,
+            [FromServices] HttpClient http,
+            [FromServices] IConfiguration config,
+            HttpContext context
+        ) =>
+        {
+            if (ttl <= 0) return Results.BadRequest();
+            var appUrl = config["AppUrl"];
+            var insId = config["InstanceId"] ?? "<Id-Not-Set>";
+            var localIp = context.Connection.LocalIpAddress?.ToString() ?? "<UNKNOWN>";
+            var thisNode = new NodeInfo(insId, localIp);
+            NodeInfo[]? result;
+            if (ttl == 1)
+            {
+                result = [thisNode];
+            }
+            else
+            {
+                var response = await http.GetFromJsonAsync<NodeInfo[]?>($"{appUrl}?ttl={ttl - 1}");
+                result = response is null ? null : [thisNode, .. response];
+            }
 
+            return Results.Ok(result);
+        }
+    )
+   .WithName("Ping")
+   .WithOpenApi();
 
-app.MapPost("/batch", async (
-    [FromBody] BatchRequest request,
-    [FromServices] IConfiguration config,
-    HttpContext context) =>
-{
-    var appUrl = config["AppUrl"];
-    var insId = config["InstanceId"] ?? "<Id-Not-Set>";
-    var localIp = context.Connection.LocalIpAddress?.ToString() ?? "<UNKNOWN>";
-    var thisNode = new NodeInfo(insId, localIp);
-    var bag = new List<BatchResponseDetail>();
-    var ping = new PingMessage(request.Ttl, 0, thisNode, thisNode);
-    for (int i = 0; i < request.BatchSize; i++)
-    {
-        var start = DateTime.Now;
-        var response = await new HttpClient().PostAsJsonAsync(appUrl, ping);
-        var end = DateTime.Now;
-        var pong = await response.Content.ReadFromJsonAsync<PongMessage>();
-        bag.Add(new(pong!.ThisNode, (long)((end - start).TotalMilliseconds)));
-    }
-
-    var total = bag.Count;
-    return new BatchResponse(
-    bag.GroupBy(d => d.FinalNode)
-        .ToDictionary(k => $"[{k.Key.Id},{k.Key.Ip}]",
-            k => new BatchResponseStats(k.Count(), (double)k.Count() / total)), bag);
-
-}).WithName("Batch").WithOpenApi();
 
 app.Run();
 
-
 public record NodeInfo(string Id, string Ip);
-public record PingMessage(int Ttl, int Count, NodeInfo Initial, NodeInfo Sender);
-public record PongMessage(int Count, NodeInfo Initial, NodeInfo LastNode, NodeInfo ThisNode);
-public record BatchRequest(int Ttl, int BatchSize);
-public record BatchResponseStats(int Count, double Ratio);
-public record BatchResponseDetail(NodeInfo FinalNode, long Time);
-public record BatchResponse(Dictionary<string, BatchResponseStats> Stats, List<BatchResponseDetail> Details);
